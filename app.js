f74752749f27b9dd6fe3c6412661c68499be8e72
syedgulzar786 <syedgulzarmohammad123@gmail.com>
2024-05-19 04:25:07 +0500

    assignment started

---
+++ app.js
@@ -0,0 +1,21 @@
+    var del = document.getElementById("del").value
+    var mod = document.getElementById("mod").value
+    var svn = document.getElementById("svn").value
+    var eit = document.getElementById("eit").value
+    var nin = document.getElementById("nin").value
+    var ash = document.getElementById("ash").value
+    var fur = document.getElementById("fur").value
+    var fiv = document.getElementById("fiv").value
+    var six = document.getElementById("six").value
+    var mul = document.getElementById("mul").value
+    var one = document.getElementById("one").value
+    var two = document.getElementById("two").value
+    var tre = document.getElementById("tre").value
+    var nus = document.getElementById("nus").value
+    var ero = document.getElementById("ero").value
+    var eql = document.getElementById("eql").value
+    var pls = document.getElementById("pls").value
+
+    function eqll(express) {
+console.log(express)
+  }
